1999018d7a68bd7b86b73ba57266ce815ca9b46b
sawanoboly <sawanoboriyu@higanworks.com>
2012-01-26 13:29:11 +0000

    Initial Commit.

---
+++ smartos/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "HiganWorks LLC"
+maintainer_email "sawanoboriyu@higanworks.com"
+license          "Apache 2.0"
+description      "Installs/Configures smartos"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.0"
